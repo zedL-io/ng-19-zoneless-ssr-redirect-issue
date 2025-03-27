8d1bfd83185be02617f3ecc745875c5cdc27b1bc
René Fabel <rene.fabel@fresh-byte.de>
2025-03-27 14:33:50 +0100

    new @let syntax

--- projects/ng-nineteen-ssr/src/app/app.component.ts
+++ projects/ng-nineteen-ssr/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NewsComponent } from "./news/news.component";
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NewsComponent],
+  imports: [RouterOutlet, NewsComponent, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,4 +16,6 @@ export class AppComponent {
   changeTitle() {
     this.title.set('ng-nineteen-ssr 2');
   }
+
+  books = signal([{id: 1, name: 'Dune', price: 2.34}, {id: 2, name: 'Dune 2', price: 2.78}, {id: 3, name: 'Dune 3', price: 2.65}, ])
 }
