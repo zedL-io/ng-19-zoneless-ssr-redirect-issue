8e4a152921c6440e752818208ee5e7baf2e6c0ce
René Fabel <rene.fabel@fresh-byte.de>
2025-03-31 19:16:07 +0200

    cc

--- projects/ng-nineteen-ssr/src/app/books/services/books.service.spec.ts
+++ projects/ng-nineteen-ssr/src/app/books/services/books.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BooksService } from './books.service';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 
 describe('BooksService', () => {
   let service: BooksService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideExperimentalZonelessChangeDetection()],
+    });
     service = TestBed.inject(BooksService);
   });
 
