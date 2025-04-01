5dfb1096bd14a50b5d697392703f0864c1db770a
René Fabel <rene.fabel@fresh-byte.de>
2025-04-01 13:57:30 +0200

    cc

--- projects/ng-nineteen-ssr/src/server.ts
+++ projects/ng-nineteen-ssr/src/server.ts
@@ -100,65 +100,6 @@ app.use('/**', async (req, res, next) => {
 });
 
 /**
- * Handle all other requests by rendering the Angular application and set CSP header and nonce in the html response.
- */
-// app.use(async (req, res, next) => {
-//   try {
-//     // create a nonce
-//     const nonce = crypto
-//       .randomBytes(16)
-//       .toString('base64')
-//       .replace(/\+/g, '-')
-//       .replace(/\//g, '_')
-//       .replace(/=+$/, '');
-
-//     // Waiting for the response from Angular SSR
-//     const renderResponse = await angularApp.handle(req);
-
-//     if (!renderResponse || !renderResponse.text) {
-//       // If no HTML is returned, send 500 error response
-//       return res.status(500).send('SSR Rendering failed');
-//     }
-
-//     // Extract the HTML
-//     const htmlResponse = await renderResponse.text();
-
-//     // Add nonce to <script> and <style> tags
-//     const updatedHtml = htmlResponse
-//       .replace(/<script.*?>/g, (match) => {
-//         return match.replace(/<script/g, `<script nonce="${nonce}"`);
-//       })
-//       .replace(/<style.*?>/g, (match) => {
-//         return match.replace(/<style/g, `<style nonce="${nonce}"`);
-//       });
-
-//     // Set the Content-Security-Policy header
-//     const cspHeader = [
-//       "default-src 'self';",
-//       `script-src 'self' 'nonce-${nonce}';`,
-//       `style-src 'self' 'nonce-${nonce}';`,
-//       "img-src 'self' data:;",
-//       "connect-src 'self' https://api.example.com;",
-//       "frame-src 'self' https://identity-provider.com;",
-//       "object-src 'none';",
-//       "base-uri 'self';",
-//       "form-action 'self';",
-//     ]
-//       .join(' ')
-//       .replace(/\s+/g, ' ');
-
-//     res.setHeader('Content-Security-Policy', cspHeader);
-
-//     // Send the response to the client
-//     res.send(updatedHtml); // `return` hinzufügen, damit der Codepfad endet
-//   } catch (error) {
-//     next(error); // Fehler an den nächsten Middleware-Handler weitergeben
-//   } finally {
-//     return;
-//   }
-// });
-
-/**
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
